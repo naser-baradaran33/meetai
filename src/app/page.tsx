a758fb36d8cc8f4057999f5e7823af5d586e6a06
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-04-19 17:41:10 +0200

    add setup 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-4xl font-bold text-green-500">
-      Hello World
-    </div>
+    <Button >
+      Click me
+    </Button>
     )
 }
